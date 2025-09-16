1d91e511ffd6c0cdc25cf3b5b521891b342471ad
Varsha Singh <varsha.s@razorpay.com>
2025-09-16 16:32:12 +0530

    feat: add solution for two-crystal-balls problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const totalFloors = breaks.length;
+    const jumpStep = Math.floor(Math.sqrt(totalFloors));
 
+    let currentFloor = jumpStep;
+
+    for (; currentFloor < breaks.length; currentFloor += jumpStep) {
+        if (breaks[currentFloor]) {
+            break;
+        }
+    }
+
+    currentFloor -= jumpStep;
+
+    for (let i = 0; i < jumpStep; ++i, ++currentFloor) {
+        if (breaks[currentFloor]) {
+            return currentFloor;
+        }
+    }
+    return -1;
 }
+
+two_crystal_balls([false, false, false, true, true]);
